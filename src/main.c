8f884dc107e510c19f841f5ba596939e27409f00
khasizadaj <javid.khasizada@gmail.com>
2024-02-13 18:43:57 +0100

    Add logic to get PATH from environment

--- src/main.c
+++ src/main.c
@@ -6,16 +6,52 @@
 /*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 20:30:33 by jkhasiza          #+#    #+#             */
-/*   Updated: 2024/02/13 18:17:04 by jkhasiza         ###   ########.fr       */
+/*   Updated: 2024/02/13 18:43:26 by jkhasiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
+#include <stdio.h>
 
-int	main(void)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	printf("Plumbing now\n");
-	putchar(65);
-	printf("\nPlumbing done.\n");
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (i < n && s1[i] != '\0' && s2[i] != '\0')
+	{
+		if ((unsigned char) s1[i] != (unsigned char) s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
+	}
+	if (i < n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
+}
+
+char	*get_path(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			return (envp[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	printf("argc = %d\n", argc);
+	for (int i = 0; argv[i]; i++)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+	printf("PATH = %s\n", get_path(envp));
 	return (0);
 }
